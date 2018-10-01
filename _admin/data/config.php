37b05988162906bf40373a65650b5936782ad5d2
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-10-01 11:37:10 +0800

    Update config.php

--- _admin/data/config.php
+++ _admin/data/config.php
@@ -1,11 +1,11 @@
 <?php return array (
   'password' => 'cyf',
-  'host' => 'http://zh.m.wikipedia.org/',
+  'host' => 'https://cyffxdl.herokuapp.com/proxy/https://www.youtube.com/',
   'replaceDomain' => '0',
   'relativeHTML' => '0',
   'relativeCSS' => '0',
   'static' => '0',
-  'diyStatic' => 'css|js|html|png|jpg|mp4|gif',
+  'diyStatic' => 'css|js|html|png|jpg|mp4|gif|mp3|htm|bmp|ico|icon|json|txt',
   'cookies' => '0',
   'agent' => '0',
   'referer' => '0',
